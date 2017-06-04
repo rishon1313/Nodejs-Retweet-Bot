7800ea5263aea7ee85d17205ecc61d80ce414e43
rishon1313 <rishon1313@gmail.com>
2017-06-04 15:54:44 +0530

    Created configuration file.

---
+++ config.js
@@ -0,0 +1,7 @@
+//Add the keys after creating your twitter application. Use https://apps.twitter.com/app/new
+module.exports = {
+  consumer_key: '',  
+  consumer_secret: '',
+  access_token: '',  
+  access_token_secret: ''
+}
